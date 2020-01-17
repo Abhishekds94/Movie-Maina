89930fda34fd846b9b8014832f9c61910a223959
Abhishek D S <abhishekds94@gmail.com>
2020-01-17 13:20:00 -0600

    RecycerView

---
+++ app/src/main/java/com/abhishek/moviemania/Movie.java
@@ -0,0 +1,53 @@
+package com.abhishek.moviemania;
+
+public class Movie {
+
+    private String title;
+    private String posterPath;
+    private String overview;
+    private String voteAverage;
+    private String releaseDate;
+
+    public Movie() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getVoteAverage() {
+        return voteAverage;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public void setVoteAverage(String voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
