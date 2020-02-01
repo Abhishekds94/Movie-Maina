abc6f0db172450bfae5d51a42b8afe233df453b7
Abhishek D S <abhishekds94@gmail.com>
2020-02-01 12:31:04 -0600

    DB package

---
+++ app/src/main/java/com/abhishek/moviemania/database/AppDatabase.java
@@ -0,0 +1,34 @@
+package com.abhishek.moviemania.database;
+
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {FavoriteEntry.class}, version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static final String LOG_TAG = AppDatabase.class.getSimpleName();
+    private static final Object LOCK = new Object();
+    private static final String DATABASE_NAME = "favorite";
+    private static AppDatabase sInstance;
+
+    public static AppDatabase getInstance(Context context){
+        if (sInstance == null){
+            synchronized (LOCK) {
+                Log.e(LOG_TAG, "Creating DB");
+                sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                        AppDatabase.class, AppDatabase.DATABASE_NAME)
+                        .build();
+            }
+        }
+        Log.e(LOG_TAG,"Getting DB instance");
+        return sInstance;
+    }
+
+    public abstract FavoriteDao favoriteDao();
+
+}
---
+++ app/src/main/java/com/abhishek/moviemania/database/FavoriteDao.java
@@ -0,0 +1,37 @@
+package com.abhishek.moviemania.database;
+
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+@Dao
+public interface FavoriteDao {
+
+    @Query("SELECT * FROM favoritetable")
+    LiveData<List<FavoriteEntry>> loadAllFavorite();
+
+    @Query("SELECT * FROM favoritetable WHERE title = :title")
+    List<FavoriteEntry> loadAll(String title);
+
+    @Insert
+    void insertFavorite(FavoriteEntry favoriteEntry);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateFavorite(FavoriteEntry favoriteEntry);
+
+    @Delete
+    void deleteFavorite(FavoriteEntry favoriteEntry);
+
+    @Query("DELETE FROM favoritetable WHERE movieid = :movie_id")
+    void deleteFavoriteWithId(int movie_id);
+
+    @Query("SELECT * FROM favoritetable WHERE id = :id")
+    LiveData<FavoriteEntry> loadFavoriteById(int id);
+}
